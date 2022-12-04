362047d03482611179cf727965d422cf80d7360f
gabrielviegas20 <gabriel.viegastrab@gmail.com>
2022-12-04 19:44:50 -0300

    Initial commit

---
+++ JavaServer.java
@@ -0,0 +1,203 @@
+import java.io.*;  
+import java.net.*;
+import java.util.Random; 
+public class JavaServer {
+    public static int GenerateRandom(){
+        int min = 1;       
+        int max = 5;
+        int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
+        
+        return random_int;
+    }
+    public static String escolha(int random){
+        String opcaoServer=null;
+        if (random == 1){
+        opcaoServer = "Pedra";
+        }
+        else{
+            if (random == 2){
+                opcaoServer = "Papel";
+            }
+            else {
+                if (random == 3){
+                    opcaoServer = "Tesoura";
+                }
+                else{
+                    if (random == 4){
+                        opcaoServer = "Lagarto";
+                    }
+                    else{
+                        opcaoServer = "Spock";
+                    }
+                }
+            }
+        }
+        return opcaoServer;
+    }
+    public static String Winner(String myChoice, String msgCliente){
+        /*#Tesoura corta papel
+        #Papel cobre pedra
+        #Pedra esmaga lagarto
+        #Lagarto envenena Spock
+        #Spock esmaga (ou derrete) tesoura
+        #Tesoura decapita lagarto
+        #Lagarto come papel
+        #Papel refuta Spock
+        #Spock vaporiza pedra
+        #Pedra amassa tesoura
+        */
+        String winner = null;
+        if (myChoice.equals("Tesoura")){
+            if(msgCliente.equals("Papel")){
+                winner = "Servidor";
+            }
+            else{
+                if(msgCliente.equals("Lagarto")){
+                    winner = "Servidor";
+                }
+                else{
+                    if(msgCliente.equals("Tesoura")){
+                        winner = "Empate";
+                    }
+                    else{
+                        winner = "Cliente";
+                    }
+                }
+            }
+        }
+        if (myChoice.equals( "Papel")){
+            if(msgCliente.equals("Pedra")){
+                winner = "Servidor";
+            }
+            else{
+                if(msgCliente.equals("Spock")){
+                    winner = "Servidor";
+                }
+                else{
+                    if(msgCliente.equals("Papel")){
+                        winner = "Empate";
+                    }
+                    else{
+                        winner = "Cliente";
+                    }
+                }
+            }
+        }
+        if (myChoice.equals( "Pedra")){
+            if(msgCliente.equals("Tesoura")){
+                winner = "Servidor";
+            }
+            else{
+                if(msgCliente.equals("Lagarto")){
+                    winner = "Servidor";
+                }
+                else{
+                    if(msgCliente.equals("Pedra")){
+                        winner = "Empate";
+                    }
+                    else{
+                        winner = "Cliente";
+                    }
+                }
+            }
+        }
+        if (myChoice.equals( "Lagarto")){
+            if(msgCliente.equals("Spock")){
+                winner = "Servidor";
+            }
+            else{
+                if(msgCliente.equals("Papel")){
+                    winner = "Servidor";
+                }
+                else{
+                    if(msgCliente.equals("Lagarto")){
+                        winner = "Empate";
+                    }
+                    else{
+                        winner = "Cliente";
+                    }
+                }
+            }
+        }
+        if (myChoice.equals( "Spock")){
+            if(msgCliente.equals("Tesoura")){
+                winner = "Servidor";
+            }
+            else{
+                if(msgCliente.equals("Pedra")){
+                    winner = "Servidor";
+                }
+                else{
+                    if(msgCliente.equals("Spock")){
+                        winner = "Empate";
+                    }
+                    else{
+                        winner = "Cliente";
+                    }
+                }
+            }
+        }
+
+        return winner;
+    }
+
+    public static void main(String[] args) {
+        try{ 
+            /*Inicio da conexão */
+            ServerSocket serverSocket = new ServerSocket(12345);
+            Socket soc = serverSocket.accept();
+            System.out.println("Receive new connection: " + soc.getInetAddress());
+            DataOutputStream dout=new DataOutputStream(soc.getOutputStream());  
+            int countClient = 0;
+            int countServer = 0;
+            int countEmpate = 0;
+
+            for(int i=0;i<15;i++){
+                DataInputStream in = new DataInputStream(soc.getInputStream());
+                int randomNum = GenerateRandom();
+                String myChoice = escolha(randomNum);
+                dout.writeUTF(myChoice);
+                String msgCliente=(String)in.readUTF();
+                /*IMPRIMIR PLACAR */
+                System.out.println("Rodada "+(i+1));
+                System.out.println(myChoice+" X "+msgCliente);
+                String winner = Winner(myChoice, msgCliente); /*Verifica quem ganhou a rodada */
+                System.out.println("Vencedor: "+ winner);
+                System.out.println("-----------------------------------------------------");
+                /*CONTAGEM DAS VITORIAS */
+                if(winner.equals("Cliente")){
+                    countClient++;
+                }
+                else{
+                    if(winner.equals("Servidor")){
+                        countServer++;
+                    }
+                    else{
+                        countEmpate++;
+                    }
+                }
+                
+            }
+        if(countClient<countServer){
+            System.out.println("Vencedor final foi o Servidor com "+countServer+" vitorias, "+countEmpate+" empates e "+countClient+" derrotas");
+        }
+        else{
+            if(countClient>countServer){
+                System.out.println("Vencedor final foi o Cliente com "+countClient+" vitorias, "+countEmpate+" empates e "+countServer+" derrotas");
+            }
+            else{
+                System.out.println("Houve um empate com o Servidor tendo "+countServer+" vitorias, "+countEmpate+" empates e "+countClient+" derrotas");
+            }
+        }
+
+        /*Encerrar conexão*/
+        dout.flush();
+        dout.close();
+        soc.close();
+        }
+        catch(Exception e)
+        {
+        e.printStackTrace(); 
+    }
+  }
+    }
---
+++ PythonCliente.py
@@ -0,0 +1,125 @@
+import jpysocket
+import socket
+import random
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+
+def mm2p(milimetros):
+    return milimetros/0.352777
+
+
+
+def checkWinner(mensagemEnvioClient, msgrecv):
+    #Tesoura corta papel
+    #Papel cobre pedra
+    #Pedra esmaga lagarto
+    #Lagarto envenena Spock
+    #Spock esmaga (ou derrete) tesoura
+    #Tesoura decapita lagarto
+    #Lagarto come papel
+    #Papel refuta Spock
+    #Spock vaporiza pedra
+    #Pedra amassa tesoura
+    vencedor='Erro'
+    if mensagemEnvioClient == 'Tesoura':
+        if msgrecv=='Papel':
+            vencedor= 'Cliente'
+        elif msgrecv == 'Lagarto':
+            vencedor= 'Cliente'
+        elif msgrecv == 'Tesoura':
+            vencedor = 'Empate'
+        else:
+            vencedor= 'Servidor'
+    elif mensagemEnvioClient=='Papel':
+        if msgrecv=='Pedra':
+            vencedor='Cliente'
+        elif msgrecv=='Spock':
+            vencedor='Cliente'
+        elif msgrecv == 'Papel':
+            vencedor = 'Empate'
+        else:
+            vencedor='Servidor'
+    elif mensagemEnvioClient == 'Pedra':
+        if msgrecv == 'Tesoura':
+            vencedor='Cliente'
+        elif msgrecv == 'Lagarto':
+            vencedor='Cliente'
+        elif msgrecv == 'Pedra':
+            vencedor = 'Empate'
+        else:
+            vencedor='Servidor'
+    elif mensagemEnvioClient == 'Lagarto':
+        if msgrecv == 'Spock':
+            vencedor='Cliente'
+        elif msgrecv == 'Papel':
+            vencedor='Cliente'
+        elif msgrecv == 'Lagarto':
+            vencedor = 'Empate'
+        else:
+            vencedor='Servidor'
+    elif mensagemEnvioClient == 'Spock':
+        if msgrecv == 'Tesoura':
+            vencedor='Cliente'
+        elif msgrecv == 'Pedra':
+            vencedor='Cliente'
+        elif msgrecv == 'Spock':
+            vencedor = 'Empate'
+        else:
+            vencedor='Servidor'       
+
+    return vencedor
+
+
+##Conexão com o Servidor
+host='localhost' #Host Name
+port=12345    #Port Number
+s=socket.socket() #Create Socket
+s.connect((host,port)) #Connect to socket
+print("Socket Is Connected....")
+countClient = 0
+countServer = 0
+countEmpate = 0
+
+pdf = canvas.Canvas("boletim_pdf.pdf")
+
+for i in range(0,15):
+    #Enviar a jogada
+    opcoesJogadas = ['Pedra', 'Papel', 'Tesoura', 'Lagarto', 'Spock']
+    mensagemEnvioClient = random.choice(opcoesJogadas)
+    msgrecv=s.recv(1024) #Recieve msg
+    msgrecv=jpysocket.jpydecode(msgrecv) #Decript msg of server
+    msgsend=jpysocket.jpyencode(mensagemEnvioClient) #Encript my Msg 
+    s.send(msgsend) #Send Msg
+    #escrever quem é o vencedor
+    print(f"Rodada {i+1}: ")
+    pdf.drawString(10 if i<10 else mm2p(110),mm2p(290-20*i) if i<10 else mm2p(290-20*(i-10)),f"Rodada {i+1}")
+    print(f"{mensagemEnvioClient} X {msgrecv}")
+    pdf.drawString(10 if i<10 else mm2p(110),mm2p(290-20*i-5) if i<10 else mm2p(290-20*(i-10)-5),f"{mensagemEnvioClient} X {msgrecv}")
+    vencedor = checkWinner(mensagemEnvioClient, msgrecv)
+    print(f"Vencedor: {vencedor}")
+    pdf.drawString(10 if i<10 else mm2p(110),mm2p(290-20*i-10) if i<10 else mm2p(290-20*(i-10)-10),f"Vencedor: {vencedor}")
+    if vencedor == "Cliente":
+        countClient=countClient+1
+    elif vencedor == "Servidor":
+        countServer=countServer+1
+    else:
+        countEmpate = countEmpate + 1
+    print("-----------------------------------------------------")
+    pdf.drawString(10 if i<10 else mm2p(110),mm2p(290-20*i-15) if i<10 else mm2p(290-20*(i-10)-15),"-----------------------------------------------------")
+
+    ##print("From Server: ",msgrecv)
+if countServer<countClient:
+    print(f"Vencedor final foi o Cliente com {countClient} vitorias, {countEmpate} empates e {countServer} derrotas")
+    pdf.drawString(mm2p(45),mm2p(75),f"Vencedor final foi o Cliente com {countClient} vitorias, {countEmpate} empates e {countServer} derrotas")
+
+elif countServer>countClient:
+    print(f"Vencedor final foi o Servidor com {countServer} vitorias, {countEmpate} empates e {countClient} derrotas")
+    pdf.drawString(mm2p(45),mm2p(75),f"Vencedor final foi o Servidor com {countServer} vitorias, {countEmpate} empates e {countClient} derrotas")
+
+elif countServer == countClient:
+    print(f"Houve um empate com o Cliente tendo {countClient} vitorias, {countEmpate} empates e {countServer} derrotas")
+    pdf.drawString(mm2p(45),mm2p(75),f"Houve um empate com o Cliente tendo {countClient} vitorias, {countEmpate} empates e {countServer} derrotas")
+
+pdf.save()
+s.close() #Close connection
+print("Connection Closed.")
